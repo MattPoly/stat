040d61d47ac4cbaaa8aef502eb8c47fabfd4bdd2
Romain.leduc <romain.leduc@outlook.fr>
2018-01-17 16:59:26 +0100

    commit1

--- app.R
+++ app.R
@@ -11,38 +11,77 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 500,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
-
+  
+  
+  # NavBar
+  shinyUI(navbarPage("Nos lois",
+                     tabPanel("Loi Normale",  
+                              textInput(inputId = "titre_histo", 
+                                        label = "Entrez le titre souhaité pour l'histogramme :",
+                                        value = "Histogramme Loi Normale"),    sidebarLayout(
+                                          sidebarPanel(
+                                            sliderInput("n_obs",
+                                                        "Nombre d'échantillons :",
+                                                        min = 1,
+                                                        max = 500,
+                                                        value = 30)
+                                          ),  mainPanel(
+                                            tabsetPanel(
+                                              tabPanel("Graphique", plotOutput("histoNormale"))
+                                            )
+                                          )
+                                        )
+                     ),
+                     tabPanel("Loi de Poisson",  
+                              textInput(inputId = "titre_histo", 
+                                        label = "Entrez le titre souhaité pour l'histogramme :",
+                                        value = "Histogramme Loi de Poisson"),    sidebarLayout(
+                                          sidebarPanel(
+                                            numericInput(inputId = "nb",
+                                                         label = "Probabilité :",
+                                                         value = "0",
+                                                         min = 0, max = 100),
+                                            numericInput(inputId = "nb_max",
+                                                         label = "Probabilité max:",
+                                                         value = "0",
+                                                         min = 0, max = 100),
+                                            numericInput(inputId = "l",
+                                                         label = "Lambda :",
+                                                         value = "5",
+                                                         min = 0, max = 100)
+                                          ),  mainPanel(
+                                            tabsetPanel(
+                                              tabPanel("Graphique", plotOutput("histoPoisson"))
+                                            )
+                                          )
+                                        )
+                     )
+  )
+  )         
+)                
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  
+  output$histoPoisson <- renderPlot({ # on fait appel à la fonction 'renderPlot' cette fois car notre sortie sera un graphique
+    set.seed(123) # ce paramètre sert à fixer une graine pour l'aléatoire lors de la génération de x (non indispensable)
+    
+    x = dpois(input$nb:input$nb_max, input$l) # x suit une loi poisson et est constitué du nombre d'observations 'n_obs' spécifié par l'utilisateur dans la partie 'UI'
+
+    hist(x, xlab = "x", ylab = "Fréquence",
+         main = input$titre_histo, # le titre de notre histogramme (paramètre 'main') va être constitué du texte rentré à la main par l'utilisateur dans la partie 'UI' et stocké dans 'titre_histo'
+         col = "skyblue", border = "white")
+  })
+  
+  output$histoNormale <- renderPlot({ # on fait appel à la fonction 'renderPlot' cette fois car notre sortie sera un graphique
+    set.seed(123) # ce paramètre sert à fixer une graine pour l'aléatoire lors de la génération de x (non indispensable)
+    
+    x = rnorm(input$n_obs) # x suit une loi normale et est constitué du nombre d'observations 'n_obs' spécifié par l'utilisateur dans la partie 'UI'
+    
+    hist(x, xlab = "x", ylab = "Fréquence",
+         main = input$titre_histo, # le titre de notre histogramme (paramètre 'main') va être constitué du texte rentré à la main par l'utilisateur dans la partie 'UI' et stocké dans 'titre_histo'
+         col = "skyblue", border = "white")
+  })
+  
 }
 
 # Run the application 
